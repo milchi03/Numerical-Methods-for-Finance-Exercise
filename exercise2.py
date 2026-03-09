4373f6ff87993f8cd1682a8b1a20837013d571a5
milchi03 <135802781+milchi03@users.noreply.github.com>
2026-03-09 23:16:57 +0100

    solved residual problems

--- exercise2.py
+++ exercise2.py
@@ -11,20 +11,22 @@ def kappa_integral(x,y):
 
 
 def build_massMatrix(N):
-    return  np.diag([1]*N) #bc integral (dirac * dirac = 1 if i=j, 0 else)
+    h = 1 / (N + 1)
+    main_diag = np.full(N, 2 * h/3)
+    off_diag = np.full(N-1, h/6)
+    return np.diag(main_diag) + np.diag(off_diag, 1) + np.diag(off_diag, -1)
 
-print(build_massMatrix(3))
+#print(build_massMatrix(3))
 
 def build_rigidityMatrix(N):
     # todo 3 b)
     # Be careful with the indices!
     # kappa_integral could be helpful here
     # phi'_i(x) = 1/h on (i-1,i) and -1/h on (i,i+1)
-    h = 1/N  
+    h = 1/(N+1)
     A = np.zeros((N,N))
     
     x = np.linspace(0, 1, N+2)
-    print(x)
 
     # The case of i=j
     for i in range(N): #Int(kappa*(1/h)**2) = (1/h)**2 * kappa_integral
@@ -43,43 +45,66 @@ def build_rigidityMatrix(N):
 
     return A
 
-print(build_rigidityMatrix(3))
+#print(build_rigidityMatrix(3))
 
 def f(t,x):
-    # todo 3 c)
-    return
-    
+    return ((x+1)*np.pi**2 - 1) * np.exp(-t) * np.sin(np.pi*x) - np.pi * np.exp(-t) * np.cos(np.pi*x)
 
 def initial_value(x):
-    # todo 3 c)
-    return
+    return np.array([np.sin(np.pi*x_i) for x_i in x])
 
 
 def exact_solution_at_1(x):
-    # todo 3 c)
-    return
+    return np.array([np.exp(-1) * np.sin(np.pi*x_i) for x_i in x])
 
 
 def build_F(t,N):
-    # todo 3 d)
-    return
+    h = 1/(N+1)
+    x = np.linspace(0,1, N+2)
 
+    F_all = (h / 3) * (f(t, x - h/2) + f(t, x) + f(t, x + h/2))
 
-def FEM_theta(N,M,theta):
-    # todo 3 e)
-    return
+    return F_all[1:-1]
+
+#print(build_F(1,3))
+
+def FEM_theta(N, M, theta):
+    Mmat = build_massMatrix(N)
+    Amat = build_rigidityMatrix(N)
+    h = 1 / (N + 1)
+    k = 1 / M       
+    t_line = np.linspace(0, 1, M + 1)
+    x = np.linspace(h, 1-h, N)
+
+    u = initial_value(x)
+
+    LHS = Mmat + k * theta * Amat
+    RHS_matrix = Mmat - k * (1 - theta) * Amat
+
+    for m in range(M):
+        f_now = build_F(t_line[m], N)
+        f_next = build_F(t_line[m+1], N)
+        
+        b = RHS_matrix @ u + k * (theta * f_next + (1 - theta) * f_now)
+        
+        u = lin.solve(LHS, b)
+        
+    return u
+
+print(FEM_theta(3,9,0.5))
 
 
 #### error analysis ####
+# only for theta = 0.5 and 1 we get a convergent solution. As with the FDM. For theta == 1 the error term depreciates lineraly, while for theta == 0.5 it depreciates quadratically. Especially easy to see for nb_samples == 7.
+# when M is written with the 4, instead of 2 the convergence happens again only at theta == 0.5 and 1. But this time the convergence rate is linear, independently of theta. This is because this time the spatial error is dominant.
 nb_samples = 5
-N = 1# fill in this line for f)-g)
-M = 1# fill in this line for f)-g)
-theta= 1# fill in this line for f)-g)
+N = np.array([2**l-1 for l in range(2,nb_samples+2)])
+M = np.array([4**l for l in range(2,nb_samples+2)]) # M = np.array([2**l for l in range(2,nb_samples+2)])
 
 
 #### Do not change any code below! ####
 l2error = np.zeros(nb_samples) 
-k =  1 / M
+k =  np.array([1 / m for m in M])
 
 try:
    for i in range(nb_samples):
