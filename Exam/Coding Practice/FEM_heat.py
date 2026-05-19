a21c251a61da65a76d7970c05b83c980bea1c0ee
milchi03 <135802781+milchi03@users.noreply.github.com>
2026-05-19 22:22:33 +0200

    mid_2_ex2

--- Exam/Coding Practice/FEM_heat.py
+++ Exam/Coding Practice/FEM_heat.py
@@ -24,7 +24,7 @@ def build_massMatrix(N):
     M_mat = sp.csc_matrix(M_mat)
     return M_mat
 
-def build_rigidityMatrix(N):
+def build_rigidityMatrix(N): #fix this!!
 #     # todo 3 b)
 #     # Be careful with the indices!
 #     # kappa_integral could be helpful here
@@ -51,55 +51,93 @@ def build_rigidityMatrix(N):
     for i in range(N-1):
         A_mat[i, i+1] = del_phi_del_phi(i, i+1, N) * kappa_integral((i+1)*h, (i+2)*h)
 
+    A_mat = sp.csc_matrix(A_mat)
     return A_mat
 
 def f(t,x):
-    return (-1 +(x+1) * np.pi**2) * np.exp**(-t) * np.sin(np.pi*x) - np.pi* np.exp**(-t) * np.cos(np.pi*x)
+    return (-1 +(x+1) * np.pi**2) * np.exp(-t) * np.sin(np.pi*x) - np.pi* np.exp(-t) * np.cos(np.pi*x)
 
-# u(t,x) = exp**(-t) * sin(pi*x)
-# del_t u = - exp**(-t) * sin(pi*x)
-# del_x u = exp**(-t) * cos(pi*x) * pi
-# I := kappa * del_x u = exp**(-t) * cos(pi*x) * pi * (x+1)
-# del_x (I) = exp**(-t) * (-sin(pi*x)) * pi**2 * (x+1) + exp**(-t) * cos(pi*x) * pi
-# =  -(x+1) * pi**2 * exp**(-t) * sin(pi*x) + pi* exp**(-t) * cos(pi*x)
+# u(t,x) = exp(-t) * sin(pi*x)
+# del_t u = - exp(-t) * sin(pi*x)
+# del_x u = exp(-t) * cos(pi*x) * pi
+# I := kappa * del_x u = exp(-t) * cos(pi*x) * pi * (x+1)
+# del_x (I) = exp(-t) * (-sin(pi*x)) * pi**2 * (x+1) + exp(-t) * cos(pi*x) * pi
+# =  -(x+1) * pi**2 * exp(-t) * sin(pi*x) + pi* exp(-t) * cos(pi*x)
 
-# del_t u - del_xx u = - exp**(-t) * sin(pi*x)  + (x+1) * pi**2 * exp**(-t) * sin(pi*x) - pi* exp**(-t) * cos(pi*x)
-# = (-1 +(x+1) * pi**2) * exp**(-t) * sin(pi*x) - pi* exp**(-t) * cos(pi*x) = f(t,x) => check
+# del_t u - del_xx u = - exp(-t) * sin(pi*x)  + (x+1) * pi**2 * exp(-t) * sin(pi*x) - pi* exp(-t) * cos(pi*x)
+# = (-1 +(x+1) * pi**2) * exp(-t) * sin(pi*x) - pi* exp(-t) * cos(pi*x) = f(t,x) => check
 
-# u_0(x) = sin(pi*x) = u(0,x) = exp**(0) * sin(pi*x) => check
+# u_0(x) = sin(pi*x) = u(0,x) = exp(0) * sin(pi*x) => check
 
-# u(t,0) = exp**(-t) * sin(0) = 0 
-# u(t,1) = exp**(-t) * sin(pi) = 0 => check
+# u(t,0) = exp(-t) * sin(0) = 0 
+# u(t,1) = exp(-t) * sin(pi) = 0 => check
 
 def initial_value(x):
     u0 = np.sin(np.pi*x)
     return u0
 
 def exact_solution_at_1(x):
+    u1 = np.exp(-1) * np.sin(np.pi*x)
+    return u1
+
+def build_F(t,N):
+    h = 1/(N+1)
+
+    F = np.zeros(N)
+
+    for i in range(0, N):
+        s1 = f(t, (i+1)*h - h/2)
+        s2 = f(t, (i+1)*h)
+        s3 = f(t, (i+1)*h + h/2)
+
+        F[i] = h * (s1 + s2 +s3)/3
+
+    return F
+
+
+def FEM_theta(N,M,theta):
+    # (M + k*theta*A)u**(m+1) = (M - k*(1-theta)*A)u**m + k*(theta * F**(m+1) + (1-theta) * F**m)
     
-    return
+    h = 1/(N+1)
+    k = 1/M
 
+    M_mat = build_massMatrix(N)
+    A_mat = build_rigidityMatrix(N)
 
-# def build_F(t,N):
-#     # todo 3 d)
-#     return
+    x = np.linspace(0,1, N+2)[1:N+1]
 
+    u_est = initial_value(x)
+    B = M_mat + k*theta*A_mat
+    C = M_mat - k*(1-theta)*A_mat
 
-# def FEM_theta(N,M,theta):
-#     # todo 3 e)
-#     return
+    for m in range(M-1):
+        print(f'Iteration {m}: u_est = \n{u_est}')
+        F_m = build_F(k*(m+1), N)
+        F_m1 = build_F(k*(m+2), N)
+        F_vec = k*theta*F_m1 + k*(1-theta)*F_m
 
+        RHS = C @ u_est + F_vec
 
-# #### error analysis ####
+        u_est = spsolve(B, RHS)
+        
+        
+    return u_est
+
+print(f'Final: u_est = \n{FEM_theta(4,4, 0.5)}') 
+print(f'Actual at 1: u = \n{exact_solution_at_1(np.array([0.2, 0.4, 0.6, 0.8]))}')
+
+# # #### error analysis ####
 # nb_samples = 5
-# N = # fill in this line for f)-g)
-# M = # fill in this line for f)-g)
-# theta= # fill in this line for f)-g)
+# N = [2**l-1 for l in range(2, 2+nb_samples)]
+# M = [2**l for l in range(2, 2+nb_samples)]
+# theta = 0.5
 
+# #for theta < 1/2 we require: k/h**2 <= 1/(2*(1-2*theta))
+# #
 
-# #### Do not change any code below! ####
+# # #### Do not change any code below! ####
 # l2error = np.zeros(nb_samples) 
-# k =  1 / M
+# k =  np.array([1 / M[j] for j in range(len(M))])
 
 # try:
 #    for i in range(nb_samples):
